b042d22ec684903a1abac4964999e66bf6bfd3cf
Mariana Santos <mariana2.santos@alunos.ifsuldeminas.edu.br>
2026-03-23 07:31:09 -0300

    aula 2 exercicio

--- aula2/exercicio.py
+++ aula2/exercicio.py
@@ -8,13 +8,37 @@ a mensagem aprovado, senão que ele foi reprovado.
 '''
 nota1 = float(input("Digite sua nota 1: "))
 nota2 = float(input("Digite sua nota 2: "))
-media = (nota1 + nota2) /2
-if media > 6 :
-    print("Aprovado")
+media = (nota1 + nota2) / 2
+if media >= 6:
+    print("Aluno aprovado")
+else:
+    exame_final = float(input("Digite a nota do exame final:"))
+    nova_media = (media + exame_final) / 2
+
+    if nova_media >= 6:
+        print ("aluno aprovado")
+    else:
+        print("reprovado")
+
+'''
+media = (nota1 + nota2) / 2
+if media >= 6:
+    print("Aluno aprovado")
+else:
+    exame_final = float(input("Digite a nota do exame final"))
+    nova_media = (media + exame_final) / 2
+
+    if nova_media >= 6:
+        print ("aluno aprovado")
+    else:
+        print("reprovado")
+
+           print("Aprovado")
 elif media <6 :
     exf = float(input("Digite sua nota do exame final: "))
-    media_exf = ((media + exf) /2)
-if media_exf > 6:
+    mediaexf = ((media + exf) /2)
+if mediaexf > 6:
     print("Voce foi aprovado")
 else :
     print("Voce foi reprovado")
+'''
